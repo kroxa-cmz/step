8a32abe8edd6db4faec01126189ef6fb02d33816
kroxa <kroxacmz@gmail.com>
2017-04-09 18:00:26 +0200

    ggg

--- etc/hello.py
+++ etc/hello.py
@@ -1,7 +1,9 @@
- def application(environ, start_response):
-     data = b"Hello, World!\n"
-	     start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(data)))
-        ])
- return iter([data])
+def application(environ, start_response):
+	data = b"Hello, World!\n"
+	start_response("200 OK", [
+    ("Content-Type", "text/plain"),
+    ("Content-Length", str(len(data)))
+    ])
+    resp = environ['QUERY_STRING'].split("&")
+    resp = [item+"\r\n" for item in resp]
+	return iter([resp])
